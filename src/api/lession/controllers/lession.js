13c641fed07435b9ed6cf1a4ba3aa139695b0e23
nguyen ngoc khanh <84921391+hellokhanhne@users.noreply.github.com>
2023-05-08 00:51:45 +0700

    add api backend

--- src/api/lession/controllers/lession.js
+++ src/api/lession/controllers/lession.js
@@ -10,7 +10,18 @@ module.exports = createCoreController("api::lession.lession", ({ strapi }) => ({
   async findOne(ctx) {
     const entry = await strapi.db.query("api::lession.lession").findOne({
       where: { id: ctx.request.params.id },
-      populate: ["questions"],
+      populate: {
+        questions: {
+          populate: {
+            word_medias: {
+              populate: {
+                url: true,
+              },
+            },
+            media: true,
+          },
+        },
+      },
     });
     if (!entry) {
       throw new Error("Couldn't find entry");
