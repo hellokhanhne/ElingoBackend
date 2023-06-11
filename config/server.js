738f386c6286918ef1b872150c9b408786ef0e73
nguyen ngoc khanh <84921391+hellokhanhne@users.noreply.github.com>
2023-06-11 21:49:05 +0700

    cm

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "https://api-1b6w.onrender.com"),
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
